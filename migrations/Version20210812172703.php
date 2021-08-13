7d75fe028d7e4747ca155c4a8d7516c8895cf8d0
Usvaandr <andrius.usvaltas@gmail.com>
2021-08-13 11:24:05 +0300

    New database created and Publisher, Game entities created.

---
+++ migrations/Version20210812172703.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210812172703 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, publisher_id_id INT NOT NULL, name VARCHAR(255) NOT NULL, year INT NOT NULL, INDEX IDX_232B318C8AAA43D0 (publisher_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE publisher (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, value TINYTEXT NOT NULL, country TINYTEXT NOT NULL, year INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE game ADD CONSTRAINT FK_232B318C8AAA43D0 FOREIGN KEY (publisher_id_id) REFERENCES publisher (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE game DROP FOREIGN KEY FK_232B318C8AAA43D0');
+        $this->addSql('DROP TABLE game');
+        $this->addSql('DROP TABLE publisher');
+    }
+}
---
+++ src/Entity/Game.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GameRepository::class)
+ */
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=publisher::class, inversedBy="games")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $publisher_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getPublisherId(): ?publisher
+    {
+        return $this->publisher_id;
+    }
+
+    public function setPublisherId(?publisher $publisher_id): self
+    {
+        $this->publisher_id = $publisher_id;
+
+        return $this;
+    }
+
+}

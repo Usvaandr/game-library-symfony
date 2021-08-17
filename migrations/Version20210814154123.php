97c8f67286f68b3984555f2959550c099eca77df
Usvaandr <andrius.usvaltas@gmail.com>
2021-08-17 18:26:59 +0300

    Added additional game to the database via migration.

--- migrations/Version20210814154123.php
+++ migrations/Version20210814154123.php
@@ -22,11 +22,14 @@ final class Version20210814154123 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('INSERT INTO publisher (name, value, country, year) 
                             VALUES ("Valve", "10bn", "USA", 1996)');
+        $this->addSql('INSERT INTO game (name, year, publisher_id) 
+                            VALUES ("Half-Life", 1998, 3)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DELETE FROM game WHERE name="Half-Life"');
         $this->addSql('DELETE FROM publisher WHERE name="Valve"');
     }
 }

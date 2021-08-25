2c6ff55329ef50cd7fee0d538763a610c575067c
Usvaandr <andrius.usvaltas@gmail.com>
2021-08-25 17:35:01 +0300

    Created templates for publishers and games using twig.

--- src/Controller/PublishersController.php
+++ src/Controller/PublishersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
+use App\Repository\PublisherRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +11,60 @@ use Symfony\Component\Routing\Annotation\Route;
 class PublishersController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @var PublisherRepository
+     */
+    private $publisherRepository;
+
+    /**
+     * @var GameRepository
+     */
+    private $gameRepository;
+
+    public function __construct(
+        PublisherRepository $publisherRepository,
+        GameRepository $gameRepository)
+    {
+        $this->publisherRepository = $publisherRepository;
+        $this->gameRepository = $gameRepository;
+    }
+
+    /**
+     * @Route("/", name="app_homepage")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new homepage controller!',
-            'path' => 'src/Controller/PublishersController.php',
+        $publishers = [
+            'Valve',
+            'Sony',
+            'Nintendo'
+        ];
+        $games = [
+            'Half Life',
+            'Last Of Us',
+            'Mario Cart'
+        ];
+
+        return $this->render('home.html.twig', [
+            'title' => 'List of publishers!',
+            'publishers' => $publishers,
+            'games' => $games
+        ]);
+    }
+
+    /**
+     * @Route("/games", name="app_games")
+     */
+    public function show(): Response
+    {
+        $games = [
+            'Half Life',
+            'Last Of Us',
+            'Mario Cart'
+        ];
+
+        return $this->render('games.html.twig', [
+            'title' => 'List of publishers!',
+            'games' => $games
         ]);
     }
 }

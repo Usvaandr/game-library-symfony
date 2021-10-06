b6a31eb05566eb0f8f87b903b29e6de5d7094c2f
Usvaandr <andrius.usvaltas@gmail.com>
2021-10-06 18:35:07 +0300

    Installed webpack  and added jquery. Refactored css and js to work with webpack.

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+var $ = require( "jquery" );
+
 const exit = e => {
     const shouldExit =
         [...e.target.classList].includes('exit-intent-popup') || // user clicks on mask
@@ -29,3 +44,7 @@ setTimeout(() => {
     document.addEventListener('mouseout', mouseEvent);
     document.addEventListener('keydown', exit);
 }, 5_000);
+
+$(document).ready(function(){
+    console.log("jQuery working properly");
+});

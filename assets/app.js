00b72282ed64cc00e8f5112b5957f7e668ea20bd
Andrius Usvaltas <andrius.usvaltas@hostinger.com>
2021-10-10 18:48:42 +0300

    0928_JS_implementation (#10)
    * 1. JS Popup.
    
    * Installed webpack  and added jquery. Refactored css and js to work with webpack.
    
    * 2. Added jquery sliders for DeletedList tables.
    
    * Fixed small mistakes.

---
+++ assets/app.js
@@ -0,0 +1,54 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+var $ = require( "jquery" );
+
+const exit = e => {
+    const shouldExit =
+        [...e.target.classList].includes('exit-intent-popup') || // user clicks on mask
+        e.target.className === 'close' || // user clicks on the close icon
+        e.keyCode === 27; // user hits escape
+
+    if (shouldExit) {
+        document.querySelector('.exit-intent-popup').classList.remove('visible');
+    }
+};
+
+const mouseEvent = e => {
+    const shouldShowExitIntent =
+        !e.toElement &&
+        !e.relatedTarget &&
+        e.clientY < 10;
+
+    if (shouldShowExitIntent) {
+        document.removeEventListener('mouseout', mouseEvent);
+        document.querySelector('.exit-intent-popup').classList.add('visible');
+    }
+};
+
+document.querySelector('.exit-intent-popup').addEventListener('click', exit);
+
+setTimeout(() => {
+    document.addEventListener('mouseout', mouseEvent);
+    document.addEventListener('keydown', exit);
+}, 5_000);
+
+$(".js-slideButton1").click(function() {
+    $(this).html() == "-" ? $(this).html("+") : $(this).html("-");
+    $(".js-slideBox1").slideToggle();
+});
+
+$(".js-slideButton2").click(function() {
+    $(this).html() == "-" ? $(this).html("+") : $(this).html("-");
+    $(".js-slideBox2").slideToggle();
+});
